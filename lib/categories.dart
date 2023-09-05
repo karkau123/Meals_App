36b41c1d73342bf8258c5ed66ae1e14b608cb2e5
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-09-05 11:17:49 +0530

    All the crid buttons are now clikable

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+// below functiom will push a screen on top of a bunch of screens
+  void _selectCategory(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) =>MealsScreen(title: 'Random Title', meals: meals[]) ,));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Pick your category'),
+        title: const Text('Pick your category'),
       ),
-      body:  GridView(
+      body: GridView(
         padding: const EdgeInsets.all(24),
-        gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-           childAspectRatio: 3/2,
-           crossAxisSpacing: 20,
-         mainAxisSpacing: 20,
-           ),
-      
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
         children: [
-           for (final category in availableCategories)
-             CategoryGridItem(category: category,)
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+            )
         ],
-
       ),
-
     );
   }
 }
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,6 +1,4 @@
- 
-
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
@@ -9,22 +7,30 @@ class CategoryGridItem extends StatelessWidget {
   final Category category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-         gradient:LinearGradient(
-          colors: [
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end:Alignment.bottomRight,
-         )
+    return InkWell(
+      onTap: (){},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius:BorderRadius.circular(16),
+
+
+      child: Container(
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+           gradient:LinearGradient(
+            colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end:Alignment.bottomRight,
+           )
+        ),
+        child: Text(category.title , style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onBackground ,
+        ),),
+        
       ),
-      child: Text(category.title , style: Theme.of(context).textTheme.titleLarge!.copyWith(
-        color: Theme.of(context).colorScheme.onBackground ,
-      ),),
-      
     );
   }
 }
