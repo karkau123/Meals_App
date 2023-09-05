9f0e3c77bcead3ed020411201e3cca749037a996
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-09-05 15:38:07 +0530

    New Screen Added for each category

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
@@ -7,11 +8,16 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
 // below functiom will push a screen on top of a bunch of screens
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+   final filteredMeals =  dummyMeals.where((meal)
+    => meal.categories.contains(
+      category.id)).toList();
+
     Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => MealsScreen(title: 'Random Title', meals: []),
+          builder: (context) => 
+          MealsScreen(title: category.title, meals:filteredMeals),
         ));
   }
 
@@ -34,7 +40,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context,category);
               },
             )
         ],
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -12,8 +13,8 @@ class MealsScreen extends StatelessWidget {
     // TODO: implement build
     Widget content = ListView.builder(
       itemCount: meals.length ,
-      itemBuilder: (context, index) => Text(meals[index].title),
-    );
+      itemBuilder: (context, index) => MealItem(meal: meals[index],
+    ));
 
     if (meals.isEmpty) {
       content = Center(
