c67dc475fa6e59d2871bcc81fb396e00af997ef2
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-11-11 20:21:40 +0530

    using provider and consumer in riverpod

--- lib/provider/favourites_provider.dart
+++ lib/provider/favourites_provider.dart
@@ -1,9 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/models/meal.dart';
 
-class FavouriteMealsNotifier extends StateNotifier<List<Meal>>{
+class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavouriteMealsNotifier() : super([]);
 
+  void toggleMealFavouriteStatus(Meal meal) {
+    final mealIsFavourite = state.contains(meal);
 
+    if (mealIsFavourite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+
+    state = [];
+  }
 }
 
-final favouriteMealsprovider = StateNotifierProvider();
+final favouriteMealsprovider =
+    StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>((ref) {
+  return FavouriteMealsNotifier();
+});
