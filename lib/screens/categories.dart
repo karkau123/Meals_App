b0d06c4a17cd16bd35f327ed4f883cead973e0b1
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-11-14 23:12:26 +0530

    rotating transition added to star

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -79,12 +79,11 @@ class _CategoriesScreenState extends State<CategoriesScreen>
               )
           ],
         ),
-        builder: (context, child) => SlideTransition(position:_animationController.drive(
-          Tween(
+        builder: (context, child) => SlideTransition(position: Tween(
             begin:const Offset(0,0.3),
             end: const Offset(0, 0),
-          ),
-        ),child: child,),
+          ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeOut),)
+          ,child: child,),
     );
   }
 }
